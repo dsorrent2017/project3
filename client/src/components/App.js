5b5fb653d78e4ca683ce6c3a2925e98396d40000
Cantando <65988365+Cantando@users.noreply.github.com>
2020-08-18 23:40:09 -0400

    added reservation and contact

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -18,6 +18,7 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRoute from "./PrivateRoute";
 import NoMatch from "../pages/NoMatch";
+import Contact from '../pages/Contact';
 
 export const App = () => {
 
@@ -43,6 +44,7 @@ export const App = () => {
                     <Route path="/login" component={Login} />
                     <Route path="/register" component={Register} />
                     <Route path="/menu" exact component={Menu} />
+                    <Route path="/contact" exact component={Contact} />
                     <Route path="/reservation" exact component={Reservation} />
                     <PrivateRoute path="/dashboard" component={UserDashboard} />
                     <PrivateRoute path="/pageone" component={PageOne} />
