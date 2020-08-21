7e5bc02dbe95998a4dbb15c03ea6ec45d528fc37
Cantando <65988365+Cantando@users.noreply.github.com>
2020-08-21 09:50:07 -0400

    trying to fix my work

---
+++ client/src/pages/Catering/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "../../components/Footer";
+import './catering.css'
+import { Card, Button } from "reactstrap";
+
+
+function Catering() {
+    return (
+
+
+        <div className="Catering">
+            <h2>Catering Packages</h2>
+
+        </div>
+
+
+    )
+}
+export default Catering;
